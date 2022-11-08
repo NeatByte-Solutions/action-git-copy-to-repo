aa1c6a86c2cf230f33d8d378f0cda33db2b52578
neatbyte-vpikulyk <viktor.pikulyk@neatbyte.solutions>
2022-11-08 17:14:20 +0200

    Added clone source logic

--- action/dist/index.js
+++ action/dist/index.js
@@ -49,18 +49,131 @@ module.exports =
 /************************************************************************/
 /******/ ({
 
+/***/ 87:
+/***/ (function(module) {
+
+module.exports = require("os");
+
+/***/ }),
+
+/***/ 129:
+/***/ (function(module) {
+
+module.exports = require("child_process");
+
+/***/ }),
+
 /***/ 526:
-/***/ (function(__unusedmodule, exports) {
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
+exports.main = exports.exec = void 0;
+const child_process = __importStar(__webpack_require__(129));
+const fs_1 = __webpack_require__(747);
+const os_1 = __webpack_require__(87);
+const path = __importStar(__webpack_require__(622));
+const exec = async (cmd, opts) => {
+    const { log } = opts;
+    const env = (opts === null || opts === void 0 ? void 0 : opts.env) || {};
+    const ps = child_process.spawn('bash', ['-c', cmd], {
+        env: {
+            HOME: process.env.HOME,
+            ...env,
+        },
+        cwd: opts.cwd,
+        stdio: ['pipe', 'pipe', 'pipe'],
+    });
+    const output = {
+        stderr: '',
+        stdout: '',
+    };
+    // We won't be providing any input to command
+    ps.stdin.end();
+    ps.stdout.on('data', (data) => {
+        output.stdout += data;
+        log.log(`data`, data.toString());
+    });
+    ps.stderr.on('data', (data) => {
+        output.stderr += data;
+        log.error(data.toString());
+    });
+    return new Promise((resolve, reject) => ps.on('close', (code) => {
+        if (code !== 0) {
+            reject(new Error('Process exited with code: ' + code + ':\n' + output.stderr));
+        }
+        else {
+            resolve(output);
+        }
+    }));
+};
+exports.exec = exec;
 const main = async ({ env = process.env, log, }) => {
     log.log(`env`, env);
+    // const config = genConfig(env);
+    // Calculate paths that use temp diractory
+    const TMP_PATH = await fs_1.promises.mkdtemp(path.join((0, os_1.tmpdir)(), 'git-publish-subdir-action-'));
+    const REPO_TEMP = path.join(TMP_PATH, 'repo');
+    const SSH_AUTH_SOCK = path.join(TMP_PATH, 'ssh_agent.sock');
+    // Environment to pass to children
+    const childEnv = Object.assign({}, process.env, {
+        SSH_AUTH_SOCK,
+    });
+    // Clone source repo
+    log.log(`##[info] Vit Cloning the repo: git clone "${env.SRC_REPO}" "${REPO_TEMP}"`);
+    await (0, exports.exec)(`git clone "${env.SRC_REPO}" "${REPO_TEMP}"`, {
+        log,
+        env: childEnv,
+    }).catch((err) => {
+        // const s = err.toString();
+        // /* istanbul ignore else */
+        // if (config.mode === 'ssh') {
+        //   /* istanbul ignore else */
+        //   if (s.indexOf('Host key verification failed') !== -1) {
+        //     log.error(KNOWN_HOSTS_ERROR(config.parsedUrl.resource));
+        //   } else if (s.indexOf('Permission denied (publickey') !== -1) {
+        //     log.error(SSH_KEY_ERROR);
+        //   }
+        // }
+        throw err;
+    });
 };
 exports.main = main;
 
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module) {
+
+module.exports = require("path");
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = require("fs");
 
 /***/ }),
 
--- action/src/index.ts
+++ action/src/index.ts
@@ -1,7 +1,67 @@
+import * as child_process from 'child_process';
+import { stream as fgStream } from 'fast-glob';
+import fsModule, { promises as fs } from 'fs';
+import gitUrlParse from 'git-url-parse';
+import { homedir, tmpdir } from 'os';
+import * as path from 'path';
+import git from 'isomorphic-git';
+import { mkdirP } from '@actions/io';
+
 export type Console = {
   readonly log: (...msg: unknown[]) => void;
   readonly error: (...msg: unknown[]) => void;
   readonly warn: (...msg: unknown[]) => void;
+};
+
+export const exec = async (
+  cmd: string,
+  opts: {
+    env?: any;
+    cwd?: string;
+    log: Console;
+  }
+) => {
+  const { log } = opts;
+  const env = opts?.env || {};
+  const ps = child_process.spawn('bash', ['-c', cmd], {
+    env: {
+      HOME: process.env.HOME,
+      ...env,
+    },
+    cwd: opts.cwd,
+    stdio: ['pipe', 'pipe', 'pipe'],
+  });
+
+  const output = {
+    stderr: '',
+    stdout: '',
+  };
+
+  // We won't be providing any input to command
+  ps.stdin.end();
+  ps.stdout.on('data', (data) => {
+    output.stdout += data;
+    log.log(`data`, data.toString());
+  });
+  ps.stderr.on('data', (data) => {
+    output.stderr += data;
+    log.error(data.toString());
+  });
+
+  return new Promise<{
+    stderr: string;
+    stdout: string;
+  }>((resolve, reject) =>
+    ps.on('close', (code) => {
+      if (code !== 0) {
+        reject(
+          new Error('Process exited with code: ' + code + ':\n' + output.stderr)
+        );
+      } else {
+        resolve(output);
+      }
+    })
+  );
 };
 
 export interface EnvironmentVariables {
@@ -23,4 +83,40 @@ export const main = async ({
   log: Console;
 }) => {
   log.log(`env`, env);
+  // const config = genConfig(env);
+
+  // Calculate paths that use temp diractory
+
+  const TMP_PATH = await fs.mkdtemp(
+    path.join(tmpdir(), 'git-publish-subdir-action-')
+  );
+  const REPO_TEMP = path.join(TMP_PATH, 'repo');
+  const SSH_AUTH_SOCK = path.join(TMP_PATH, 'ssh_agent.sock');
+
+  // Environment to pass to children
+  const childEnv = Object.assign({}, process.env, {
+    SSH_AUTH_SOCK,
+  });
+
+  // Clone source repo
+  log.log(
+    `##[info] Vit Cloning the repo: git clone "${env.SRC_REPO}" "${REPO_TEMP}"`
+  );
+
+  await exec(`git clone "${env.SRC_REPO}" "${REPO_TEMP}"`, {
+    log,
+    env: childEnv,
+  }).catch((err) => {
+    // const s = err.toString();
+    // /* istanbul ignore else */
+    // if (config.mode === 'ssh') {
+    //   /* istanbul ignore else */
+    //   if (s.indexOf('Host key verification failed') !== -1) {
+    //     log.error(KNOWN_HOSTS_ERROR(config.parsedUrl.resource));
+    //   } else if (s.indexOf('Permission denied (publickey') !== -1) {
+    //     log.error(SSH_KEY_ERROR);
+    //   }
+    // }
+    throw err;
+  });
 };
