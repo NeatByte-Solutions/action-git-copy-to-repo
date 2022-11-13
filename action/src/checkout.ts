8f89322c82ffc7ab7736b52da540cbbd62291af5
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2022-11-13 11:36:47 +0200

    Refactor instructions

--- action/src/checkout.ts
+++ action/src/checkout.ts
@@ -4,6 +4,16 @@ import { exec } from './utils/processUtils';
 import { Console, Context, ExecOpts, RepoData } from './types';
 import { SSH_KEY_ERROR } from './utils/errorMessages';
 
+class NoSuchBranchError extends Error {
+  constructor(branch: string, repo: string) {
+    super(`Failed to checkout branch "${branch}" from repository "${repo}"`);
+  }
+}
+
+// use check
+// if (err instanceOf NoSuchBranchError)
+// for not existing branch cases
+
 // const checkoutTargetBranch = async ({ config, tmpFolder, childEnv, log }: CheckoutProps) => {
 //   // Check if branch already exists
 //   log.log(`##[info] Checking if branch ${config.branch} exists already`);
@@ -66,6 +76,8 @@ export const checkoutSrc = async (context: Context) => {
     execOpts: context.exec.srcExecOpt,
     log: context.log,
   });
+
+  // switch to config branch (error if failure)
 };
 
 export const checkoutTarget = async (context: Context) => {
@@ -77,6 +89,11 @@ export const checkoutTarget = async (context: Context) => {
     log: context.log,
   });
 
+  // 1) switch to config branch (can be a failure if such doesn't exist)
+  // 2) switch to "TARGET_BASE_BRANCH" (if needed otherwise stay on default branch)
+  // 3) create new branch (from default branch or defined "TARGET_BASE_BRANCH")
+
+  
   // await checkoutTargetBranch({
   //   config: config.target,
   //   tmpFolder: TARGET_REPO_TEMP,
@@ -84,3 +101,8 @@ export const checkoutTarget = async (context: Context) => {
   //   log,
   // });
 };
+
+// use same approach as "setupSshKeys = async (context: Context): Promise<void>"
+// (no need to have more code in index.ts - just pass context, all details are hidden inside step)
+// maybe better to do 2 funcs: checkoutBranch, checkoutOrCreateBranch
+// as src and target repos have different logic
