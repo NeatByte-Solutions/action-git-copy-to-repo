a0b4288dd19d0a302f69e1d3b070a5e46d2f8397
Viktor Pikulyk <91457159+neatbyte-vpikulyk@users.noreply.github.com>
2022-11-18 17:38:10 +0200

    Remove index-old (#6)

--- action/src/steps/config.ts
+++ action/src/steps/config.ts
@@ -7,7 +7,6 @@ export const config = async (env: EnvironmentVariables, context: Context): Promi
     src: {
       sshRepo: env.SRC_SSH_REPO,
       sshPrivateKey: env.SRC_SSH_PRIVATE_KEY,
-      // sshPrivateKey: env.SRC_SSH_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       githubRepo: env.SRC_GITHUB_REPO,
       githubToken: env.SRC_GITHUB_TOKEN,
       branch: env.SRC_BRANCH,
@@ -16,7 +15,6 @@ export const config = async (env: EnvironmentVariables, context: Context): Promi
     target: {
       sshRepo: env.TARGET_SSH_REPO,
       sshPrivateKey: env.TARGET_SSH_PRIVATE_KEY,
-      // sshPrivateKey: env.TARGET_SSH_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       githubRepo: env.TARGET_GITHUB_REPO,
       githubToken: env.TARGET_GITHUB_TOKEN,
       branch: env.TARGET_BRANCH,
