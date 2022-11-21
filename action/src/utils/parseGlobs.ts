8fe0b29886ce081d1bfb9b0be44fd15499884e3b
neatbyte-vpikulyk <viktor.pikulyk@neatbyte.solutions>
2022-11-21 12:53:43 +0200

    Optional branches and code style fixes

--- action/src/utils/parseGlobs.ts
+++
@@ -1,9 +0,0 @@
-export const parseGlobs = (configStr: string, defaultGlobs: string[]) => {
-  const globList = configStr
-    .toString()
-    .split('\n')
-    .map((s) => s.trim())
-    .filter((s) => s !== '');
-
-  return [...globList, ...defaultGlobs];
-};
